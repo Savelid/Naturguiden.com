629b6b6f27e0c1b4fd2215539be0e2d50afedfb2
Johsav <john@savelid.se>
2018-04-28 09:29:05 +0200

    working with inquiry form

--- src/about/post.php
+++ src/about/post.php
@@ -1,6 +1,5 @@
-<?php
-      	if (isset($_POST['Submit'])) { 
-			
+
+<?php		
 		   	$StartDate = $_POST['StartDate'];
 			$EndDate = $_POST['EndDate'];
 			$NumberOfPeople = $_POST['NumberOfPeople'];
@@ -18,28 +17,53 @@
 			$headers .= "Reply-To: ".$email." \r\n";
 			$headers .= "MIME-Version: 1.0\r\n";
 			$headers .= "Content-Type: text/html; charset=utf-8\r\n";
-			
-			$subject = "Intresse av VED3";
-			
-			$mailto = "john@savelid.se";
-			 
-			$form_message = "<html><body><p><b>".$name."</b> har skickat ett meddelande angående VED3.\n
-			<br>
-			<b>E-post:</b> ".$Email." \n 
-			<b>Telefon:</b> ".$Phone." \n
-			<br>
-			<b>Meddelande:<b/> ".$Comment." </p></body></html>"; 
-			 	 
-				 
-			$sent = mail($mailto, $subject, $form_message, $headers);
 
-			
+			/*Mail to NATURGUIDEN */
+			$subject1 = "Naturguiden-com Inquiry";
+			$mailto1 = "john@naturguiden.se";
+			 
+			$form_message1 = "<html><body><p>Message sent from Naturguiden.com Inquiry form\n<br/>
+			<b>FirstName:</b> ".$FirstName." \n
+			<b>LastName:</b> ".$LastName." \n 
+			<b>Email:</b> ".$Email." \n
+			<b>Phone:</b> ".$Phone." \n
+			<b>Country:</b> ".$Country." \n
+			<br />
+			<b>NumberOfPeople:</b> ".$NumberOfPeople." \n
+			<b>StartDate:</b> ".$StartDate." \n
+			<b>EndDate:</b> ".$EndDate." \n
+			<b>Category:</b> ".$Category." \n
+			<b>SubCategory:</b> ".$SubCategory." \n
+			<br>
+			<b>Comment:<b/> ".$Comment." </p></body></html>"; 
+	 
+			$sent = mail($mailto1, $subject1, $form_message1, $headers);
+
+			/*Mail to NATURGUIDEN */
+			$subject2 = "Confirmation from Naturguiden";
+			$mailto2 = $Email;
+			 
+			$form_message2 = "<html><body><p>Thank you ".$FirstName." for your request. We will soon come back with an answer.\n<br/>
+			<b>FirstName:</b> ".$FirstName." \n
+			<b>LastName:</b> ".$LastName." \n 
+			<b>Email:</b> ".$Email." \n
+			<b>Phone:</b> ".$Phone." \n
+			<b>Country:</b> ".$Country." \n
+			<br />
+			<b>NumberOfPeople:</b> ".$NumberOfPeople." \n
+			<b>StartDate:</b> ".$StartDate." \n
+			<b>EndDate:</b> ".$EndDate." \n
+			<b>Category:</b> ".$Category." \n
+			<b>SubCategory:</b> ".$SubCategory." \n
+			<br>
+			<b>Comment:<b/> ".$Comment." </p></body></html>"; 
+	 
+			$sent2 = mail($mailto2, $subject2, $form_message2, $headers);
+
 	   	if($sent){
 		   	echo "<p style='color: green'>Tack ".$FirstName.", ditt meddelande har skickats! Vi besvarar meddelandet så fort som möjligt.</p>"; 
 		   }
 	   	else {
 		   	echo "<p style='color: red'>Meddelandet kunde inte skickas. Vänligen försök igen eller kontakta oss via telefon eller e-post.</p>"; 
-	 }
-	 
- }
+	 	}
 ?>       
